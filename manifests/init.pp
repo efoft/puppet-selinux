07f2646c6d30882f3a65858cd3e09073633ea17f
pajan la <pajan@yandex.ru>
2017-09-25 10:36:35 +0300

    replaced validate_* in favor of datatypes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,8 @@
 #
 class selinux(
-  $status = 'enforcing',
-  $type   = 'targeted',
+  Enum['enforcing','permissive','disabled'] $status = 'enforcing',
+  Enum['targeted','minimum','mls']          $type   = 'targeted',
 ) {
-
-  validate_re($status, ['enforcing','permissive','disabled'], "${status} is not valid for status, expected values are: 'enforcing','permissive','disabled'")
-  validate_re($type, ['targeted','minimum','mls'], "${type} is not valid for status, expected values are: 'targeted','minimum','mls'")
 
   file_line { "selinux-${status}":
     path  => '/etc/selinux/config',
@@ -24,6 +21,6 @@ class selinux(
   exec { 'selinux-setenforce':
     command => $status ? { 'enforcing' => 'setenforce 1', 'permissive' => 'setenforce 0' },
     path    => ['/usr/sbin','/sbin','/usr/bin','/bin'],
-    onlyif  => 'getenforce | grep -i permissive',
+    unless  => "getenforce | grep -i ${status}",
   }
 }
